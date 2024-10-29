e12899f30b6893b6b6b5ac83eadbdf8701495c35
Guy Garcia <guy.garcia@protonmail.com>
2024-10-29 09:05:48 -0400

    deps and cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,6 @@ pub use fungible::*;
 pub use non_fungible::*;
 use serde::Serialize;
 
-// TODO: implement batch send for native token
-// TODO: implement Auth for native
-// TODO: implement Perms for Cw20
-// TODO: implement Send from Cw20 that allows sending from a message
-// TODO: implement mint for Cw20
-
 pub type TokenKey = (u8, String);
 
 pub(crate) fn execute_wasm<T: Serialize + ?Sized>(
